3dee9f33a16a41a2d86bca0d19daa9ca306d7a07
Joshua Taylor <taylorjoshua099@gmail.com>
2018-11-19 15:37:55 +1100

    Removed birthday requirement for signup

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import './Landing.css';
 import FormText from '../Forms/FormText/FormText';
-import FormDate from '../Forms/FormDate/FormDate';
+// import FormDate from '../Forms/FormDate/FormDate';
 import PubAcc from '../Common/PubAcc/PubAcc';
 import { withRouter, Redirect } from 'react-router-dom';
 import { registerUser } from '../../actions/authActions';
@@ -18,9 +18,9 @@ class Landing extends Component {
             username: "",
             password1: "",
             password2: "",
-            day: "",
-            month: "",
-            year: ""
+            day: "20", //Hardcoded because functionality was removed but the database still requires it
+            month: "09",
+            year: "1999"
         }
     }
 
@@ -38,10 +38,7 @@ class Landing extends Component {
 
         const { email, username, password1, password2 } = this.state;
 
-        //If a date dropdown field was not entered, its value is converted to an empty string to be detected invalid by the backend form validation. Otherwise, converted to DD-MM-YYYY format
-        const dob = this.state.day === "" || this.state.month === "" || this.state.year === "" ?
-            "" :
-            `${this.state.day}-${this.state.month}-${this.state.year}`;
+        const dob = `${this.state.day}-${this.state.month}-${this.state.year}`;
 
         const userData = {
             email: email.toLowerCase(),
@@ -125,7 +122,7 @@ class Landing extends Component {
                             <FormText type="text" name="username" placeholder="Username" onChange={this.onChange} value={this.state.username} errors={this.props.errors} />
                             <FormText type="password" name="password1" placeholder="Password" onChange={this.onChange} value={this.state.password1} errors={this.props.errors} />
                             <FormText type="password" name="password2" placeholder="Password" onChange={this.onChange} value={this.state.password2} errors={this.props.errors} />
-                            <FormDate name="dob" onChange={this.onChange} errors={this.props.errors} />
+                            {/* <FormDate name="dob" onChange={this.onChange} errors={this.props.errors} /> */}
                             <input type="submit" className="landing__form-btn" value="Register" />
                         </form>
                         <h2 className="landing__heading-2">
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -17,9 +17,9 @@ class Register extends Component {
       username: "",
       password1: "",
       password2: "",
-      day: "",
-      month: "",
-      year: ""
+      day: "20", //Hardcoded these because i took out this functionality but it's still required in the database.
+      month: "09",
+      year: "1999"
     }
   }
 
@@ -37,10 +37,7 @@ class Register extends Component {
 
     const { email, username, password1, password2 } = this.state;
 
-    //If a date dropdown field was not entered, its value is converted to an empty string to be detected invalid by the backend form validation. Otherwise, converted to DD-MM-YYYY format
-    const dob = this.state.day === "" || this.state.month === "" || this.state.year === "" ?
-      "" :
-      `${this.state.day}-${this.state.month}-${this.state.year}`;
+    const dob = `${this.state.day}-${this.state.month}-${this.state.year}`;
 
     const userData = {
       email: email.toLowerCase(),
@@ -72,7 +69,7 @@ class Register extends Component {
             <FormText type="text" name="username" placeholder="Username" onChange={this.onChange} value={this.state.username} errors={this.props.errors} />
             <FormText type="password" name="password1" placeholder="Password" onChange={this.onChange} value={this.state.password1} errors={this.props.errors} />
             <FormText type="password" name="password2" placeholder="Password" onChange={this.onChange} value={this.state.password2} errors={this.props.errors} />
-            <FormDate name="dob" onChange={this.onChange} errors={this.props.errors} />
+            {/* <FormDate name="dob" onChange={this.onChange} errors={this.props.errors} /> */}
             <input type="submit" className="register__form-btn" value="Register" />
           </form>
           <h2 className="register__heading-2">
